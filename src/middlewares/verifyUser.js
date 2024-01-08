59d34bdf7554d2fe03b17ad74095d7fbc388bdfd
Nitish Matiyara <91205231+NitishMatiyara@users.noreply.github.com>
2024-01-08 17:53:56 +0530

    bugfix verifyUser

--- src/middlewares/verifyUser.js
+++ src/middlewares/verifyUser.js
@@ -14,7 +14,7 @@ const verifyUser = async (req, res, next) => {
       //decodes token id
       const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-      req.user = await User.findById(decoded.id).select("-password");
+      req.user = await User.findById(decoded._id).select("-password");
 
       next();
     } catch (error) {
